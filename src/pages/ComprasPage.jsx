4ecd768c6ef3cc0acdee72e0d4fa87aa830484fc
D0dSou1 <0DodecahedronSoul1@gmail.com>
2024-02-29 11:42:06 -0500

    creacion de tarjeta
    Ademas agregue otras funcionalidades

--- src/pages/ComprasPage.jsx
+++ src/pages/ComprasPage.jsx
@@ -1,20 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { Card } from "../components/Card";
 
 export const ComprasPage = () => {
-    const [productos, setProductos] = useState([])
+  const [productos, setProductos] = useState([]);
 
-    const fetchProductos = async ()=>{
-        const response = await fetch('https://fakestoreapi.com/products')
-        const data = await response.json()
-        setProductos(data)
-        console.log(data)
-    }
+  const fetchProductos = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setProductos(data);
+    console.log(data);
+  };
 
-    useEffect(() => {
-        fetchProductos()
-    }, [])
-    
+  useEffect(() => {
+    fetchProductos();
+  }, []);
+
   return (
-    <div>ComprasPage</div>
-  )
-}
+    <>
+      <h1>Compras</h1>
+      <hr />
+      {productos.map(producto=>(
+        <Card imagen={producto.image} titulo={producto.title} descripcion={producto.description} precio={producto.price}/>
+      ))}
+      <Card/>
+    </>
+  );
+};
