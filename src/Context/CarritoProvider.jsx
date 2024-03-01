1cd0c97c57cb441acdf8a5fb8732b7a78ea71058
D0dSou1 <0DodecahedronSoul1@gmail.com>
2024-03-01 11:10:13 -0500

    Carrito Compras
    agregue algunas cosas mas

---
+++ src/Context/CarritoProvider.jsx
@@ -0,0 +1,39 @@
+import { useReducer } from "react";
+import { CarritoContext } from "./CarritoContext";
+
+const initialState = [];
+
+export const CarritoProvider = ({ children }) => {
+  const [listaCompras, dispatch] = useReducer(comprasReducer, initialState);
+
+  const agregarCompra = (compra) => {
+    const action = {
+      type: "",
+      payload: compra,
+    };
+    dispatch(action);
+  };
+
+  const aumentarCantidad = (id) => {
+    const action = {
+      type: "",
+      payload: id,
+    };
+    dispatch(action);
+  };
+  const disminuirCantidad = (id) => {
+    const action = {
+      type: "",
+      payload: id,
+    };
+    dispatch(action);
+  };
+  const eliminarCompra = (id) => {
+    const action = {
+      type: "",
+      payload: id,
+    };
+    dispatch(action);
+  };
+  return <CarritoContext.Provider>{children}</CarritoContext.Provider>;
+};
